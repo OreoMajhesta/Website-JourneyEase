7e4db9a1919976c549a7f712fece8e115f2fa199
oreo <114158979+OreoMajhesta@users.noreply.github.com>
2024-10-08 06:26:40 +0700

    Update NavBar.tsx

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+//import { useLocation } from 'react-router-dom';
 import useHandleLinkClick from '../functions/handleLinkClick';
 import { useTheme } from '../functions/ThemeContext';
 import { icons } from '../../assets/icons/icons';
